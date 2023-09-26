0fb2eaddc3f5c5f1db66946e2bffaa476d956d02
Moonshadow <last_enemy@naver.com>
2023-09-26 11:05:46 +0900

    Posts UI

---
+++ src/views/posts/PostEditView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>게시글 등록</h2>
+    <hr class="my-4" />
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">제목</label>
+      <input type="text" class="form-control" id="title" />
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">내용</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    <div class="pt-4">
+      <button type="button" class="btn btn-outline-danger me-2" @click="goDetailPage">취소</button>
+      <button type="button" class="btn btn-primary">수정</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+const id = route.params.id
+
+const goDetailPage = () => {
+  router.push({
+    name: 'PostDetail',
+    params: { id },
+  })
+}
+</script>
+
+<style lang="scss" scoped></style>
